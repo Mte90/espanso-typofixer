36073322b0ab7f6285c871e319be27395a708d5e
Daniele Scasciafratte <mte90net@gmail.com>
2024-02-20 10:48:05 +0100

    fix(review): done

--- tools/the3toterms.py
+++ tools/the3toterms.py
@@ -1,24 +1,32 @@
+#!/usr/bin/env python
 import requests
-import json
-import io
-import re
+import os
+from SyntaxAutoFix.utils import open_typo_file, save_typo_data
 
 the3_src = requests.get('https://raw.githubusercontent.com/swaits/thethethe.nvim/main/lua/thethethe/dictionary.lua')
-the3_src = the3_src.text
 
-the3 = {}
-the3_src = the3_src.split("\n")
+the3_content = the3_src.text
+the3_content = the3_content.split("\n")
 previous_word = ''
 
-for rule in the3_src:
-	if ':' in rule:
-		misspelled_word, correct_word = rule.split(':')
-		if correct_word != previous_word:
-			the3[correct_word] = [misspelled_word]
-			previous_word = correct_word
-		else:
-			the3[correct_word].append(misspelled_word)
+lang_path = os.path.dirname(os.path.realpath(__file__)) + '/../words/en.json'
 
-the3 = json.dumps(the3, indent=2)
-with open('../words/extra/the3.json', 'w', encoding='utf-8') as json_file:
-	json_file.write(the3)
+typo_data = open_typo_file(lang_path)
+print("Found %s words in english" % len(typo_data))
+before = len(typo_data)
+
+for rule in the3_content:
+    if ':' in rule:
+        misspelled_word, correct_word = rule.split(':')
+        correct_word = correct_word.lower()
+        misspelled_word = misspelled_word.lower()
+        if correct_word not in typo_data:
+            typo_data[correct_word] = [misspelled_word]
+        else:
+            if misspelled_word not in typo_data[correct_word]:
+                typo_data[correct_word].append(misspelled_word)
+
+after = len(typo_data)
+print("Found %s words on the3" % str(after - before))
+
+save_typo_data(lang_path, typo_data)
