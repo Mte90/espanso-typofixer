a68e773241b5f1a9ef83840296f371fdddc292f9
Daniele Scasciafratte <mte90net@gmail.com>
2024-06-12 15:39:18 +0200

    feat(en): imported from https://github.com/crate-ci/typos, #7

--- tools/csvtoterms.py
+++ tools/csvtoterms.py
@@ -9,19 +9,25 @@ parser = argparse.ArgumentParser(description='add new terms!')
 
 parser.add_argument('-file', dest="file", type=str, required=True)
 parser.add_argument('-lang', dest="lang", type=str, required=True)
+parser.add_argument('-opposite', dest="opposite", type=bool, required=False)
 args = parser.parse_args()
 
 script_path = os.path.dirname(os.path.realpath(__file__))
 script_path = os.path.join(script_path, '../words/' + args.lang + '.json')
 
 typo_data = open_typo_file(script_path)
-print(typo_data)
+
 with open(args.file) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
+    exact = 0
+    wrong = 1
+    if args.opposite:
+        exact = 1
+        wrong = 0
     for row in csv_reader:
-        if row[0] in typo_data:
-            typo_data[row[0]].append(row[1])
+        if row[exact] in typo_data:
+            typo_data[row[exact]].append(row[wrong])
         else:
-            typo_data[row[0]] = [row[1]]
+            typo_data[row[exact]] = [row[wrong]]
 
 save_typo_data(script_path, typo_data)
